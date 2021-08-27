9bae9fb514502bfb259186575685ef5b4480b99e
Martin <78501668+FhatersDev@users.noreply.github.com>
2021-08-27 11:24:52 -0500

    Translating
    Translating

--- src/main/java/me/martinlite/jdbiconnection/sql/ConnectionSQL.java
+++ src/main/java/me/martinlite/jdbiconnection/sql/ConnectionSQL.java
@@ -5,8 +5,8 @@ import org.jdbi.v3.core.Jdbi;
 public class ConnectionSQL {
 
     public Jdbi getConnection(){
-        SQLClient client = SQLClient.newSQLClient().ip("localhost").port("3306").database("prueba").user("root").password("admin").build(); //agregamos los parametros de la base de datos
-        Jdbi jdbi = client.getConnection(); //obtenemos la conexion
-        return jdbi; //retornamos la conexcion
+        SQLClient client = SQLClient.newSQLClient().ip("localhost").port("3306").database("prueba").user("root").password("admin").build(); //we add the data from the database
+        Jdbi jdbi = client.getConnection(); //we get the connection
+        return jdbi; //we return the connection
     }
 }
