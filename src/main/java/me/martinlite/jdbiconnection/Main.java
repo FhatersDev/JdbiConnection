ef509404fcbb0761f30391ef5b1b9798949d49ce
Martin <78501668+FhatersDev@users.noreply.github.com>
2021-08-27 13:45:16 -0500

    Added methods

--- src/main/java/me/martinlite/jdbiconnection/Main.java
+++ src/main/java/me/martinlite/jdbiconnection/Main.java
@@ -3,15 +3,58 @@ package me.martinlite.jdbiconnection;
 import me.martinlite.jdbiconnection.sql.ConnectionSQL;
 import org.jdbi.v3.core.Handle;
 import org.jdbi.v3.core.Jdbi;
+import org.jdbi.v3.core.statement.Update;
 
 public class Main {
     static ConnectionSQL connection = new ConnectionSQL();
+    Jdbi conn = connection.getConnection();////This is the connection
 
     public static void main(String[] args) {
-        Jdbi conn = connection.getConnection(); //This is the connection
-
+        // Suppose we want to create a program to get a person's money from their bank account
+        Main main = new Main();
+        int money = main.getMoney("Martin");
 
     }
 
 
+    //The first step is to create the methods to obtain the player money
+
+    public void insertUser(String username, int money) {
+        try (Handle handle = conn.open()) { //handle is the option that allows you to access the jdbi options
+            handle.createUpdate("INSERT INTO <TABLE> (name,money) VALUES (?,?)")
+                    .define("TABLE", connection.getTable())
+                    .bind(0, username)
+                    .bind(1, money)
+                    .execute();
+        }
+    }
+
+    public void deleteUser(String username) {
+        try (Handle handle = conn.open()) {
+            handle.createUpdate("DELETE FROM `<TABLE>`  WHERE `name`=? ")
+                    .define("TABLE", connection.getTable())
+                    .bind(0, username)
+                    .execute();
+        }
+    }
+    public void updateUser(String username,int money){
+        try (Handle handle = conn.open()) {
+            handle.createUpdate("UPDATE <TABLE> SET money=? WHERE name = ?")
+            .define("TABLE",connection.getTable())
+            .bind(0,money)
+            .bind(1,username)
+            .execute()
+            ;
+        }
+    }
+    public int getMoney(String username){
+        try (Handle handle = conn.open()) {
+    int money = handle.select("SELECT money FROM <TABLE> WHERE name= ? ")
+            .define("TABLE",connection.getTable())
+            .bind(0,username)
+            .mapTo(Integer.class)
+            .findOnly();
+        return money;
+        }
+    }
 }
--- src/main/java/me/martinlite/jdbiconnection/sql/ConnectionSQL.java
+++ src/main/java/me/martinlite/jdbiconnection/sql/ConnectionSQL.java
@@ -3,10 +3,38 @@ package me.martinlite.jdbiconnection.sql;
 import org.jdbi.v3.core.Jdbi;
 
 public class ConnectionSQL {
+    public String getIp() {
+        return ip;
+    }
 
+    public String getPort() {
+        return port;
+    }
+
+    public String getDatabase() {
+        return database;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+    public String getTable(){
+        return table;
+    }
+
+    String ip = "localhost";
+    String port = "3306";
+    String database = "bank";
+    String user = "root";
+    String pass = "admin";
+    String table = "banco";
     public Jdbi getConnection(){
-        SQLClient client = SQLClient.newSQLClient().ip("localhost").port("3306").database("prueba").user("root").password("admin").build(); //we add the data from the database
-        Jdbi jdbi = client.getConnection(); //we get the connection
-        return jdbi; //we return the connection
+        SQLClient client = SQLClient.newSQLClient().ip(ip).port(port).database(database).user(user).password(pass).build(); //agregamos los parametros de la base de datos
+        Jdbi jdbi = client.getConnection(); //obtenemos la conexion
+        return jdbi; //retornamos la conexcion
     }
 }
